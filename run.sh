885b436d603895b16e6873ed8977d723a00c8d78
Alan Mang <alanmang@MacBook-Pro-de-Alan-2.local>
2022-03-29 21:32:02 +0200

    Final Bash Script

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-pip install pandas
+
 VIRTUAL_ENV = venv
 if [ -d "$VIRTUAL_ENV" ]; then
 source ./venv/bin/activate
+pip install pandas
 else
 python3 -m venv venv
 source ./venv/bin/activate
+pip install pandas
 pip install --upgrade pip
 pip install -r requirements.txt
 fi
